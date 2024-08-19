2b83697472421b928dd1c769df5d886573406494
Mekhai Elijah <3lijahmekhai@gmail.com>
2024-08-18 21:20:13 -0700

    second commit, added button

---
+++ src/app/HomeButton.tsx
@@ -0,0 +1,14 @@
+function HomeButton() {
+    return (
+      <button></button>
+    );
+  }
+
+export default function Home () {
+return (
+  <div>
+    <h1>HOME</h1>
+    <HomeButton/>
+  </div>
+)
+}
